631f5a7aa9dd672a1d0dd9cc456d24a794fe33a6
Ben Shung Cano <benlongo321@gmail.com>
2024-11-24 21:12:15 -0600

    Refactored files to follow guidelines

---
+++ sudoku.py
@@ -0,0 +1,94 @@
+import pygame, random
+import sudoku_generator
+from pygments.styles.dracula import background
+
+def main():
+    # Initialize screen
+    SCREEN_WIDTH = 1280
+    SCREEN_HEIGHT = 720
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    pygame.display.set_caption("Sudoku")
+
+
+    # Fill background option 1
+    background_image = pygame.image.load('Sudoku_BG.jpg')
+    background_image = pygame.transform.scale(background_image,(1280,720))
+
+    # Fill background option 2
+    lines = [[random.uniform(0,SCREEN_WIDTH), random.uniform(-SCREEN_HEIGHT,SCREEN_HEIGHT)] for i in range(50)]
+
+    BLACK = (0,0,0)
+    WHITE = (255,255,255)
+    RED = (255,0,0)
+    PURPLE = (128,0,128)
+
+
+    # Welcome text
+    welcome_font = pygame.font.Font(None, 80)
+    welcome_text = welcome_font.render("Welcome to Sudoku!",1,WHITE)
+    welcome_textpos = welcome_text.get_rect()
+    welcome_textpos.centerx = screen.get_rect().centerx
+    welcome_textpos.centery = 200
+
+
+    # Select game mode text gm_text == gamemodetext
+    gm_font = pygame.font.Font(None, 50)
+    gm_text = gm_font.render("Select Game Mode:",1,WHITE)
+    gm_textpos = gm_text.get_rect()
+    gm_textpos.centerx = screen.get_rect().centerx
+    gm_textpos.centery = 350
+
+    # default font for button text
+    button_font = pygame.font.Font(None, 48)
+
+    # easy
+    easy_rect = pygame.Rect(300,250,320,500)
+    easy_text = button_font.render("Easy",1,WHITE)
+    easy_button = easy_text.get_rect(center=easy_rect.center)
+
+    # medium
+    medium_rect = pygame.Rect(300, 250, 640, 500)
+    medium_text = button_font.render("Medium", 1, WHITE)
+    medium_button = medium_text.get_rect(center=medium_rect.center)
+
+    # hard
+    hard_rect = pygame.Rect(300, 250, 960, 500)
+    hard_text = button_font.render("Hard", 1, WHITE)
+    hard_button = hard_text.get_rect(center=hard_rect.center)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = event.pos
+                if easy_button.collidepoint(mouse_x,mouse_y):
+                    print("Easy button pressed!")
+                if medium_button.collidepoint(mouse_x,mouse_y):
+                    print("Medium button pressed!")
+                if hard_button.collidepoint(mouse_x,mouse_y):
+                    print("Hard button pressed!")
+
+        # cool purple animated background
+        screen.fill(BLACK)
+        for line in lines:
+            pygame.draw.line(screen, PURPLE, (line[0], line[1]), (line[0], line[1] + 50), 2)
+            line[1] += .5
+            if line[1] > SCREEN_HEIGHT:
+                line[1] = random.randint(-100, 0)
+
+        # placing everything on the screen
+        screen.blit(welcome_text, welcome_textpos)
+        screen.blit(gm_text,gm_textpos)
+        pygame.draw.rect(screen,PURPLE,easy_button)
+        screen.blit(easy_text,easy_button)
+        pygame.draw.rect(screen, PURPLE, medium_button)
+        screen.blit(medium_text,medium_button)
+        pygame.draw.rect(screen, PURPLE, hard_button)
+        screen.blit(hard_text,hard_button)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
+

d74e4acd6bb116de6647fbc6415700b938d483a9
Peter <yungmanpeter@gmail.com>
2024-12-03 00:13:24 -0500

    Finished the render function of the highlight.py file which highlights the cell that the user is hovering over

--- game/systems/highlight.py
+++ game/systems/highlight.py
@@ -12,7 +12,7 @@ class HighlightSystem(System):
     def __init__(self, board_widget: SudokuBoardWidget):
         self.selected_cell = (-1,-1)
         self.board_widget = board_widget
-        self.cell_size = board_widget.get_size()[0]/board_widget.board.get_size()[0]
+        self.cell_size = board_widget.get_size()[0]/(board_widget.board.get_size()[0]+1)
         self.mouse_x = -1
         self.mouse_y = -1
     def enter_scope(self):
@@ -31,9 +31,9 @@ class HighlightSystem(System):
         top_left_x = (context.surface.get_size()[0]-self.board_widget.get_size()[0])/2
         top_left_y = (context.surface.get_size()[1]-self.board_widget.get_size()[0])/2
 
-        self.selected_cell = (int(self.mouse_x / self.cell_size)+1, int(self.mouse_y / self.cell_size)+1)
+        self.selected_cell = (int((self.mouse_x-top_left_x) / self.cell_size), int((self.mouse_y-top_left_y) / self.cell_size))
 
-        print(self.selected_cell)
+
 
 
         hovered_x = top_left_x + self.selected_cell[0]*self.cell_size
